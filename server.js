9b4160447ee2dd3fda9f2ae11bc37ffed09fd876
Anuj Singh <anujk920@gmail.com>
2020-08-22 16:58:18 +0530

    changes

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const path = require('path');
 const app = express();
 
 
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
+// const forceSSL = function() {
+//     return function (req, res, next) {
+//       if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(
+//          ['https://', req.get('Host'), req.url].join('')
+//         );
+//       }
+//       next();
+//     }
+//   }
 
-app.use(forceSSL());
+// app.use(forceSSL());
 
 app.use(express.static(__dirname + '/dist/Email-Management'));
 
